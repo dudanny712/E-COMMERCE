88657584e59c3dc479f56b765a47999a813e9fc3
dudanny712 <58696798+dudanny712@users.noreply.github.com>
2022-03-20 15:42:56 +0100

    fina

--- src/data.js
+++ src/data.js
@@ -47,7 +47,7 @@ export const sliderItems = [
     },
     {
       id:2,
-      img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+      img:"../Images/Slide3.jpg",
     },
     {
       id:3,
@@ -67,7 +67,7 @@ export const sliderItems = [
     },
     {
       id:7,
-      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+      img:"../Images/Product 7.jpg",
     },
     {
       id:8,
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Announcements from '../components/Announcements'
 import Categories from '../components/Categories'
 import Navbar from '../components/Navbar'
+import Products from '../components/Products'
 // import OpeningPage from '../components/OpeningPage'
 import Slider from '../components/Slider'
 
@@ -13,6 +14,7 @@ const Home = () => {
         <Navbar/>
         <Slider/>
         <Categories/>
+        <Products/>
         </div>
   )
 }
