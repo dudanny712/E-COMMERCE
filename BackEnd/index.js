091200feaccb560471d3d3a7fbdbbe35a16ef6e6
dudanny712 <58696798+dudanny712@users.noreply.github.com>
2022-03-23 12:00:24 +0100

    backend update

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
+const userRoute= require('./routes/user');
 
 dotenv.config();
 
@@ -11,6 +11,7 @@ mongoose.connect(
 ).then(()=> console.log("DB Connected"))
 .catch((err) => {console.log(err)});
 
+app.get("/api/user", userRoute);
 
 app.listen(process.env.PORT|| 4000, () => {
     console.log("BackEND SERVER Online");
