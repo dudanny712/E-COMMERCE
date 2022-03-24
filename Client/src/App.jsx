ec62883ccadd99288843c38e36428301a3d9bd41
dudanny712 <58696798+dudanny712@users.noreply.github.com>
2022-03-24 16:13:35 +0100

    admin update

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
@@ -7,17 +7,18 @@ import Cart from "./pages/Cart";
 import SingleProduct from "./pages/SingleProduct";
 import Register from "./pages/Register";
 import Success from "./pages/Success";
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 const App = () => {
-  // const user = useSelector((state) => state.user.currentUser);
+  const user = useSelector((state) => state.user.currentUser);
   return (
    
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route path="/products/:category" element={<ProductList/>}/>
-        {/* {user ? <Navigate  to="/" /> : */}
+        {user ? <Navigate  to="/" /> :
         <Route path="/login"   element={<Login/>}/>
+  }
         <Route path="/cart" element={<Cart/>}/>
         <Route path="/product/:id" element={<SingleProduct/>}/>
         <Route path="/register" element={<Register/>}/>
--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import styled from "styled-components";
-import {mobile} from "../responsive";
+import { login } from "../redux/apiCalls";
+import { mobile } from "../responsive";
+import { useDispatch, useSelector } from "react-redux";
 
 const Container = styled.div`
   width: 100vw;
@@ -8,7 +11,7 @@ const Container = styled.div`
       rgba(255, 255, 255, 0.5),
       rgba(255, 255, 255, 0.5)
     ),
-    url("../Images/Login.jpg")
+    url("https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
       center;
   background-size: cover;
   display: flex;
@@ -20,7 +23,6 @@ const Wrapper = styled.div`
   width: 25%;
   padding: 20px;
   background-color: white;
-  
   ${mobile({ width: "75%" })}
 `;
 
@@ -37,7 +39,7 @@ const Form = styled.form`
 const Input = styled.input`
   flex: 1;
   min-width: 40%;
-  margin: 15px 0;
+  margin: 10px 0;
   padding: 10px;
 `;
 
@@ -45,11 +47,14 @@ const Button = styled.button`
   width: 40%;
   border: none;
   padding: 15px 20px;
-  background-color: #F05454;
+  background-color: teal;
   color: white;
   cursor: pointer;
   margin-bottom: 10px;
-  border-radius: 15px;
+  &:disabled {
+    color: green;
+    cursor: not-allowed;
+  }
 `;
 
 const Link = styled.a`
@@ -59,17 +64,40 @@ const Link = styled.a`
   cursor: pointer;
 `;
 
+const Error = styled.span`
+  color: red;
+`;
+
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const { isFetching, error } = useSelector((state) => state.user);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    login(dispatch, { username, password });
+  };
   return (
     <Container>
       <Wrapper>
         <Title>SIGN IN</Title>
         <Form>
-          <Input placeholder="Username" />
-          <Input placeholder="Password" />
-          <Button>LOGIN</Button>
-          <Link>Forgot Password?</Link>
-          <Link>New?Register.</Link>
+          <Input
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+            placeholder="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button onClick={handleClick} disabled={isFetching}>
+            LOGIN
+          </Button>
+          {error && <Error>Something went wrong...</Error>}
+          <Link>Forgot PASSWORD?</Link>
+          <Link>CREATE A NEW ACCOUNT</Link>
         </Form>
       </Wrapper>
     </Container>
